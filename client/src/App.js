44f113b6bbb47e69150281fbd73174fb3553ac57
bertdida <38489033+bertdida@users.noreply.github.com>
2020-12-12 19:24:09 +0800

    Use AceEditor component

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,69 @@
 import { useState } from "react";
+import AceEditor from "react-ace";
+
+import "ace-builds/src-noconflict/mode-css";
+import "ace-builds/src-noconflict/theme-monokai";
+import "ace-builds/src-noconflict/ext-language_tools";
+
+const defaultContent = `body {
+  height: 100vh;
+  width: 100vw;
+  background-image: linear-gradient(
+    to right top,
+    #d16ba5,
+    #c777b9,
+    #ba83ca,
+    #aa8fd8,
+    #9a9ae1,
+    #8aa7ec,
+    #79b3f4,
+    #69bff8,
+    #52cffe,
+    #41dfff,
+    #46eefa,
+    #5ffbf1
+  );
+}`;
+
+export function App() {
+  const [content, setContent] = useState(defaultContent);
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    setIsLoading(true);
+
+    const { result } = await formatContent(content);
+    setIsLoading(false);
+    setContent(result);
+  }
+
+  function onChange(value) {
+    setContent(value);
+  }
+
+  return (
+    <form onSubmit={onSubmit}>
+      <button type="submit">Declare Hex Codes</button>
+      <div>
+        <AceEditor
+          mode="css"
+          theme="monokai"
+          onChange={onChange}
+          fontSize={14}
+          value={isLoading ? "⌛ transpiling..." : content}
+          readOnly={isLoading}
+          focus={true}
+          setOptions={{
+            enableLiveAutocompletion: true,
+            showLineNumbers: true,
+            useWorker: false,
+          }}
+        />
+      </div>
+    </form>
+  );
+}
 
 async function formatContent(content) {
   const requestOptions = {
@@ -9,32 +74,4 @@ async function formatContent(content) {
 
   const response = await fetch("/formatter", requestOptions);
   return response.json();
-}
-
-export function App() {
-  const [content, setContent] = useState(`body {
-    background-color: #f5f5f5;
-    color: #333;
-}`);
-
-  async function onSubmit(event) {
-    event.preventDefault();
-
-    const { result } = await formatContent(content);
-    console.log(result);
-    setContent(result);
-  }
-
-  function onChange(event) {
-    setContent(event.target.value);
-  }
-
-  return (
-    <form onSubmit={onSubmit}>
-      <button type="submit">Declare Hex Codes</button>
-      <div>
-        <textarea value={content} onChange={onChange} rows={30} cols={60} />
-      </div>
-    </form>
-  );
 }
